492ec967c98b40799c7bf4ba564d9b9df485cc76
Kai T. Ohlhus <k.ohlhus@gmail.com>
2021-02-22 11:27:42 +0900

    fix typo in prev commit

--- src/api.php
+++ src/api.php
@@ -154,7 +154,7 @@ class api
     $columns = (array_key_exists('Columns', $request))
              ? $request['Columns']
              : array_column(array_values(CONFIG::ITEM_DATA), 0);
-    if (!array_key_exists('Format', $request) {
+    if (!array_key_exists('Format', $request)) {
       return $fmt->asJSON();
     }
     switch ($request['Format']) {
